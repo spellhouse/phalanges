868820e451df7ede047ede793e7326899428dc99
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-06-29 03:34:56 -0700

    Add docstrings, reverse implementation of keyword<->keycode maps

--- src/phalanges/core.cljs
+++ src/phalanges/core.cljs
@@ -1,12 +1,16 @@
 (ns phalanges.core
+  "js/KeyboardEvent utilities."
   (:require
    [clojure.string :as string]
+   [clojure.set :as set]
    [goog.object])
   (:require-macros
    [phalanges.core :refer [define-key-code-predicates]])
   (:import goog.events.KeyCodes))
 
-(def keycode->keyword
+(def
+  ^{:doc "ClojureScript friendly version of goog.events.KeyCodes."}
+  keyword->keycode
   (reduce
     (fn [m k]
       (let [v (aget goog.events.KeyCodes k)
@@ -14,33 +18,62 @@
                   (string/replace #"_" "-")
                   (keyword))]
         (if-not (fn? v)
-          (assoc m v k)
+          (assoc m k v)
           m)))
     {}
     (array-seq (goog.object/getKeys goog.events.KeyCodes))))
 
-(def keyword->keycode
-  (into {} (map (juxt val key) keycode->keyword)))
+(def
+  ^{:doc "Inversion of keyword->keycode."}
+  keycode->keyword
+  (set/map-invert keyword->keycode))
 
-(defn key-code [e]
+(defn key-code
+  "Return the keyCode property of a js/KeyboardEvent. "
+  [e]
   (.-keyCode e))
 
-(defn control-key? [e]
-  (.-ctrlKey e))
+(defn repeating?
+  "Return the repeat property of js/KeyboardEvent. True if the key is 
+  being held down."
+  [e]
+  (.-repeat e))
 
-(defn shift-key? [e]
-  (.-shiftKey e))
+(defn control-key?
+  "Return the ctrlKey property of a js/KeyboardEvent. True if the 
+  control key is being held."
+  [e]
+  (boolean (.-ctrlKey e)))
 
-(defn alt-key? [e]
-  (.-altKey e))
+(defn shift-key?
+  "Return the shiftKey property of a js/KeyboardEvent. True if the
+  shift key is being held."
+  [e]
+  (boolean (.-shiftKey e)))
 
-(defn meta-key? [e]
-  (.-metaKey e))
+(defn alt-key?
+  "Return the altKey property of a js/KeyboardEvent. True if the
+  alt key is being held."
+  [e]
+  (boolean (.-altKey e)))
+
+(defn meta-key?
+  "Return the metaKey property of a js/KeyboardEvent. True if the
+  meta key is being held."
+  [e]
+  (boolean (.-metaKey e)))
 
 (define-key-code-predicates)
 
-(defn modifiers
-  "Given a js/KeyboardEvent return a set of keywords."
+(defn modifier-set
+  "Given a js/KeyboardEvent return a set of keywords corresponding to the
+  modifier keys involved.
+  
+
+  Ex.
+    (modifiers e)
+    ;; => #{:control :shift}
+  "
   [e]
   (let [mods {:ctrl (control-key? e)
               :shift (shift-key? e)
@@ -49,11 +82,13 @@
     (set (keys (filter val mods)))))
 
 (defn key-set
-  "Given a js/KeyboardEvent return a set of keywords.
+  "Given a js/KeyboardEvent return a set of keywords corresponding to the
+  keys pressed. Includes modifiers.
 
   Ex.
-    (key-set e) => #{:shift :a}
+    (key-set e) 
+    ;; => #{:shift :a}
   "
   [e]
-  (conj (modifiers e) (keycode->keyword (key-code e))))
+  (conj (modifier-set e) (keycode->keyword (key-code e))))
 
