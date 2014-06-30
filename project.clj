6627427bbf3fcf76382483afeb420df1351c33ee
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-06-29 22:52:28 -0700

    Version bump to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spellhouse/phalanges "0.1.0-SNAPSHOT"
+(defproject spellhouse/phalanges "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
