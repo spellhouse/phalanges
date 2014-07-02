0fe958d94104c70ce2bb272bb138b53c9bd725f8
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-07-02 09:47:59 -0700

    Add listen-sequence!/unlisten-sequence! and more predicates
    listen-sequence! provides a way to match and dispatch 
    a function over a subsequence of mapping key-set over a 
    sequence of keyboard events. This allows one to easily
    define keyboard shortcuts, etc.

--- src/phalanges/core.cljs
+++ src/phalanges/core.cljs
@@ -8,6 +8,34 @@
    [phalanges.core :refer [define-key-code-predicates]])
   (:import goog.events.KeyCodes))
 
+;; ---------------------------------------------------------------------
+;; Utilities
+
+(defn prune
+  "Recursively dissoc empty values on key-path.
+
+  Ex.
+    (prune {:a {:b {:c {}}
+                :d :foo}}
+           [:a :b :c])
+    ;; => {:a {:d :foo}}
+  "
+  [m key-path]
+  (if (seq key-path)
+    (let [key (last key-path)
+          edit-path (butlast key-path)]
+      (if (and (contains? (get-in m edit-path) key)
+               (empty? (get-in m key-path)))
+        (if edit-path
+          (recur (update-in m edit-path dissoc key)
+                 edit-path)
+          (dissoc m key))
+        m))
+    m))
+
+;; ---------------------------------------------------------------------
+;; Keyword/Keycode conversion
+
 (def
   ^{:doc "ClojureScript friendly version of goog.events.KeyCodes."}
   keyword->keycode
@@ -28,42 +56,63 @@
   keycode->keyword
   (set/map-invert keyword->keycode))
 
+;; ---------------------------------------------------------------------
+;; KeyboardEvent wrappers
+
 (defn key-code
-  "Return the keyCode property of a js/KeyboardEvent. "
+  "Return the keyCode property of a js/KeyboardEvent."
   [e]
   (.-keyCode e))
 
+(defn key-up?
+  "True if e is keyup event, false otherwise."
+  [e]
+  (= (.-type e) "keyup"))
+
+(defn key-down?
+  "True if e is keydown event, false otherwise."
+  [e]
+  (= (.-type e) "keydown"))
+
+(defn key-press?
+  "True if e is keypress event, false otherwise."
+  [e]
+  (= (.-type e) "keypress"))
+
 (defn repeating?
   "Return the repeat property of js/KeyboardEvent. True if the key is 
-  being held down."
+  being held down, false otherwise."
   [e]
   (boolean (.-repeat e)))
 
 (defn control-key?
   "Return the ctrlKey property of a js/KeyboardEvent. True if the 
-  control key is being held."
+  control key is being held, false otherwise."
   [e]
   (boolean (.-ctrlKey e)))
 
 (defn shift-key?
   "Return the shiftKey property of a js/KeyboardEvent. True if the
-  shift key is being held."
+  shift key is being held, false otherwise."
   [e]
   (boolean (.-shiftKey e)))
 
 (defn alt-key?
   "Return the altKey property of a js/KeyboardEvent. True if the
-  alt key is being held."
+  alt key is being held, false otherwise."
   [e]
   (boolean (.-altKey e)))
 
 (defn meta-key?
   "Return the metaKey property of a js/KeyboardEvent. True if the
-  meta key is being held."
+  meta key is being held, false otherwise."
   [e]
   (boolean (.-metaKey e)))
 
 (define-key-code-predicates)
+
+;; ---------------------------------------------------------------------
+;; Keyset
 
 (defn modifier-set
   "Given a js/KeyboardEvent return a set of keywords corresponding to the
@@ -92,3 +141,58 @@
   [e]
   (conj (modifier-set e) (keycode->keyword (key-code e))))
 
+;; ---------------------------------------------------------------------
+;; Key sequence listener
+
+(defn- get-register! [el]
+  (or (aget el "__phalanges_register")
+      (let [new-register (atom [])]
+        (aset el "__phalanges_register" new-register)
+        new-register)))
+
+(defn- get-fn-store! [el]
+  (or (aget el "__phalanges_fn_store")
+      (let [new-fn-store (atom {})]
+        (aset el "__phalanges_fn_store" new-fn-store)
+        new-fn-store)))
+
+(defn- get-sequence-listener! [el]
+  (or (aget el "__phalanges_sequence_listener")
+      (let [register (get-register! el)
+            fn-store (get-fn-store! el)
+            listener (fn [e]
+                       (let [key-set (keyboard/key-set e)]
+                         (swap! register conj key-set)
+                         (let [x (get-in @fn-store @register)]
+                           (if (nil? x)
+                             (reset! register [key-set])
+                             (when (fn? x)
+                               (x @register)
+                               (swap! register empty))))))]
+        (aset el "__phalanges_sequence_listener" listener)
+        listener)))
+
+(defn listen-sequence!
+  "Listen for keydown events such that whenever a sub-sequence of 
+  mapping key-set over those events matches key-sets f is called
+  with the matched sub-sequence.
+  
+  Ex.
+    ;; Typing \"a b c\" into #test-input will show \"a b c\" in the
+    ;; console.
+    (let [el (js/document.getElementById \"test-input\")]
+      (listen-sequence! el [#{:a} #{:b} #{:c}] #(js/console.log \"a b c\")))
+  "
+  [el key-sets f]
+  (swap! (get-fn-store! el) assoc-in key-sets f)
+  (events/listen el "keydown" (get-sequence-listener! el)))
+
+(defn unlisten-sequence!
+  "Stop listening for a sequence of keydown events."
+  [el key-sets]
+  (swap! (get-fn-store! el)
+         (fn [store]
+           (let [edit-path (butlast key-sets)
+                 key (last key-sets)]
+             (-> (update-in store edit-path dissoc key)
+                 (prune edit-path))))))
