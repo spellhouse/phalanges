60b4ed3bc1120533873c14f6e60dd63ce569c12b
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-07-25 21:09:43 -0700

    Version bump to 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spellhouse/phalanges "0.1.2-SNAPSHOT"
+(defproject spellhouse/phalanges "0.1.2"
   :description "ClojureScript library providing utilities for working with JavaScript KeyboardEvents."
   :url "https://github.com/spellhouse/phalanges"
   :license {:name "Eclipse Public License"
