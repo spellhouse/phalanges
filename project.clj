6ad4cc271cee7986b81bbabcf94edf2892903876
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-09-23 20:49:21 -0700

    Version bump to 0.1.4

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject spellhouse/phalanges "0.1.4-SNAPSHOT"
+(defproject spellhouse/phalanges "0.1.4"
   :description "ClojureScript library providing utilities for working with JavaScript KeyboardEvents."
   :url "https://github.com/spellhouse/phalanges"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies
-  [[org.clojure/clojure "1.5.1"]
-   [org.clojure/clojurescript "0.0-2227" :scope "provided"]]
+  [[org.clojure/clojure "1.5.1"]]
 
   :profiles
   {:dev {:source-paths ["src" "dev"]
          :dependencies [[weasel "0.2.0"]
                         [com.cemerick/piggieback "0.1.3"]
-                        [figwheel "0.1.3-SNAPSHOT"]]
+                        [figwheel "0.1.3-SNAPSHOT"]
+                        [org.clojure/clojurescript "0.0-2227" :scope "provided"]]
          :plugins [[com.cemerick/austin "0.1.3"]
                    [lein-figwheel "0.1.3-SNAPSHOT"]
                    [lein-cljsbuild "1.0.3"]]
